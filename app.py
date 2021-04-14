49b9136284e493c2d621a9f7a2bbca70ecae2019
Wassim Chegham <github@wassim.dev>
2021-04-14 21:28:14 +0200

    chore: clean setup

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+amount = 100
+tax = .06
+total = amount + amount * tax
+print(total)
